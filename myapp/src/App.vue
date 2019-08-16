adc90f4a57d1699ae1ce29703f76813e38d8ddae
MukundaHolla <kanda05holla@gmail.com>
2019-08-16 15:20:30 +0530

    Life cycle hooksgit add .

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -2,42 +2,27 @@
   <div id="app">
     <h1>{{ msg }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend"/>
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
-
+import Blogs from './Blogs';
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
+    Blogs
   },
   data () {
     return {
-      msg: 'Welcome to Mukundas Vue.js App',
-      friends: [
-                { name:'Namrata', online: true },
-                { name:'Mukunda', online: false },
-                { name:'Modi', online: true },
-                { name:'Ram', online: false },
-            ]
+      msg: 'Welcome to Mukundas Vue.js App'
     }
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+    
   }
 }
 </script>
---
+++ myapp/src/Blogs.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="blogs">
+        <h2>{{ blogTitle }}</h2>
+        <button @click="changeTitle">click me!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blogs',
+    data() {
+        return {
+            blogTitle: 'Blogs'
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.blogTitle = 'Amazing'
+        }
+    },
+    beforeCreate() {
+        alert('beforeCreate Hook')
+    },
+    created() {
+        alert('Created')
+    },
+    beforeUpdate() {
+        alert('before update')
+    }
+}
+</script>
