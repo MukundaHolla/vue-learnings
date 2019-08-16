fa80c4ca18c48469ba90a6a079cccc9f44d24b76
MukundaHolla <kanda05holla@gmail.com>
2019-08-16 15:49:35 +0530

    computed property, implemented custom search with filter

--- myapp/src/Blogs.vue
+++ myapp/src/Blogs.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="blogs">
         <h2>{{ blogTitle }}</h2>
-        <div v-for="post in posts" :key="post.id">
+        <input type="text" v-model="searchTerm">
+        <div v-for="post in filteredPosts" :key="post.id">
             <h3>{{ post.title }}</h3>
             <p>{{ post.body | snippet }}</p>
         </div>
@@ -16,11 +17,19 @@ export default {
     data() {
         return {
             blogTitle: 'Blogs',
-            posts: []
+            posts: [],
+            searchTerm: ''
         }
     },
     methods: {
     
+    },
+    computed: {
+        filteredPosts() {
+            return this.posts.filter(post => {
+                return post.title.match(this.searchTerm)
+            })
+        }
     },
     created() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
