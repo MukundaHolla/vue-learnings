19c521802590265108d8360065f12232cb335575
MukundaHolla <kanda05holla@gmail.com>
2019-08-16 15:34:33 +0530

    added axios for api call

--- myapp/src/Blogs.vue
+++ myapp/src/Blogs.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="blogs">
         <h2>{{ blogTitle }}</h2>
-        <button @click="changeTitle">click me!</button>
+        <div v-for="post in posts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Blogs',
     data() {
         return {
-            blogTitle: 'Blogs'
+            blogTitle: 'Blogs',
+            posts: []
         }
     },
     methods: {
-        changeTitle() {
-            this.blogTitle = 'Amazing'
-        }
-    },
-    beforeCreate() {
-        alert('beforeCreate Hook')
+    
     },
     created() {
-        alert('Created')
-    },
-    beforeUpdate() {
-        alert('before update')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response)
+            this.posts = response.data;
+        }).catch(err => {
+            console.log(err)
+        })
     }
 }
 </script>
